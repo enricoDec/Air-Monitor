9fb75ca65a9afa456cceccfb68f9e80b197de478
enricoDec <enricodechadarevian@gmail.com>
2021-04-04 20:13:14 +0200

    Initial commit

---
+++ src/main/java/htw/ai/akma/airmonitor/AirMonitorApplication.java
@@ -0,0 +1,13 @@
+package htw.ai.akma.airmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/htw/ai/akma/airmonitor/AirMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.ai.akma.airmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
